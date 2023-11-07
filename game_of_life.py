ed7f788819ad865e2d77efa92460ba665671a15c
anusha <anushakizhakkeveedu@gmail.com>
2023-11-07 22:51:25 +0530

    GREEN:Fix Error on test_update_grid_die due to overpopulation

--- game_of_life.py
+++ game_of_life.py
@@ -18,5 +18,16 @@ def update_grid(grid):
                 for y in [-1, 0, 1]:
                     if x == 0 and y == 0:
                         continue  # Skip the current cell
-                   
+                    new_i, new_j = i + x, j + y
+                    if 0 <= new_i < HEIGHT and 0 <= new_j < WIDTH:
+                        live_neighbors += grid[new_i][new_j]
+
+            if grid[i][j] == 1:
+                if live_neighbors < 2 or live_neighbors > 3:
+                    new_grid[i][j] = 0
+                else:
+                    new_grid[i][j] = 1
+            else:
+                if live_neighbors == 3:
+                    new_grid[i][j] = 1
     return new_grid
