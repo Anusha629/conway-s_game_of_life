6c75fefc2860b193606c9add668dfa9a0e772c21
anusha <anushakizhakkeveedu@gmail.com>
2023-11-07 22:41:42 +0530

    GREEN:test for update_grid_no_changes (when the grid remains the same)

--- game_of_life.py
+++ game_of_life.py
@@ -9,4 +9,14 @@ def get_empty_grid(width, height):
         grid.append(row)
     return grid
 
-
+def update_grid(grid):
+    new_grid = get_empty_grid(WIDTH, HEIGHT)
+    for i in range(HEIGHT):
+        for j in range(WIDTH):
+            live_neighbors = 0
+            for x in [-1, 0, 1]:
+                for y in [-1, 0, 1]:
+                    if x == 0 and y == 0:
+                        continue  # Skip the current cell
+                   
+    return new_grid
