17a788e66af635ffb30fc5d8b0dd022644fd076a
anusha <anushakizhakkeveedu@gmail.com>
2023-11-07 22:35:15 +0530

    RED: test for update_grid_no_changes

--- game_of_life.py
+++ game_of_life.py
@@ -1,9 +1,12 @@
 
 WIDTH, HEIGHT = 40, 40
 
+
 def get_empty_grid(width, height):
     grid = []
     for _ in range(height):
         row = [0] * width
         grid.append(row)
     return grid
+
+
--- test_game_of_life.py
+++ test_game_of_life.py
@@ -8,3 +8,11 @@ def test_get_empty_grid():
     grid = game_of_life.get_empty_grid(0, 0)
     expected_grid = []
     assert grid == expected_grid
+
+def test_update_grid_no_changes():
+    game_of_life.HEIGHT = 3
+    game_of_life.WIDTH = 3
+    grid = game_of_life.get_empty_grid(game_of_life.WIDTH, game_of_life.HEIGHT)
+    expected_grid = game_of_life.get_empty_grid(game_of_life.WIDTH, game_of_life.HEIGHT)
+    new_grid = game_of_life.update_grid(grid)
+    assert new_grid == expected_grid
